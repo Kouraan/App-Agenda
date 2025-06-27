1e7e89dcce06ff95baff78759b461a4fdb19cbe4
Kouraan <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>
2025-06-27 11:49:05 +0100

    -P inicio de JavaFX

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Vamos la fazer esta merda funcionar!");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 300, 200);
-        primaryStage.setTitle("Agenda");
+    public void start(Stage primaryStage) throws Exception {
+
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLUE); // Cor do BackGround
+
+        Image icon = new Image("../resources/icon.png"); //puxar uma imagem de um diretorio
+        primaryStage.getIcons().add(icon); // Adicionar o icon a janela
+        primaryStage.setTitle("App Agenda"); // Mudar o titulo da janela
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds(); // Pegar o tamanho da tela
+        primaryStage.setWidth(screenBounds.getWidth()); // Mudar a largura da janela
+        primaryStage.setHeight(screenBounds.getHeight()); // Mudar a altura da janela
+        primaryStage.setResizable(false); // Tornar a janela não redimensionável
+        
         primaryStage.setScene(scene);
         primaryStage.show();
     }
